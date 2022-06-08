0c436cd0794a407585615d5010733b05e21873b0
Kalvin <kalvinz61@gmail.com>
2022-06-08 18:28:10 -0400

    added components CurrentGuess PreviousGuess EmptyGuess

--- components/Wordle/wordle.tsx
+++ components/Wordle/wordle.tsx
@@ -4,14 +4,13 @@ import styles from "./wordle.module.css";
 
 export default function Wordle() {
 	const [word, setWord] = useState("");
-	const [guess, setGuess] = useState<Array<string>>([]);
-	const [submittedGuesses, setSubmittedGuesses] = useState<Array<Array<string>>>([]);
+	const [guess, setGuess] = useState<string[]>([]);
+	const [submittedGuesses, setSubmittedGuesses] = useState<string[][]>([]);
 	const [winOrLose, setWinOrLose] = useState<boolean>(false);
 	//listening for keydown
 	useEffect(() => {
 		function handleKeydown({ key }: { key: string }) {
 			const isChar = /^[a-zA-Z]$/.test(key);
-			console.log(key);
 			//only add to array if it is a letter and less than 5 curent letters
 			if (isChar && guess.length < 5) {
 				setGuess((prev) => [...prev, key.toUpperCase()]);
@@ -39,15 +38,12 @@ export default function Wordle() {
 
 	//checking for game over
 	useEffect(() => {
-		console.log(guess);
-		console.log(submittedGuesses);
-		console.log(word);
 		if (submittedGuesses.length === 6 && winOrLose === false) {
 			window.alert("game over");
 		} else if (winOrLose) {
 			window.alert("You Win!");
 		}
-	}, [submittedGuesses, winOrLose, guess, word]);
+	}, [submittedGuesses, winOrLose]);
 
 	//Get word
 	useEffect(() => {
@@ -57,24 +53,61 @@ export default function Wordle() {
 		};
 		fetchData();
 	}, []);
+
+	//logging
+	useEffect(() => {}, [word, submittedGuesses]);
+
 	return (
 		<div className={styles.root}>
 			<h1>WORDLE-REP</h1>
-			<div className={styles.word}>
-				{/* generate 5 empty boxes and only fill them if there is a curent guess at that index */}
-				{Array.from({ length: 5 }).map((letter, idx) => {
-					return (
-						<div className={styles.letterContainer} key={idx}>
-							{guess[idx] || ""}
-						</div>
-					);
-				})}
-			</div>
-			<div>
-				{/* {Array.from({ length: 5 }).map((letter, idx) => {
-					return <div className={styles.letterContainer} key={idx}></div>;
-				})} */}
-			</div>
+			<PreviousGuesses submittedGuesses={submittedGuesses} />
+			<CurrentGuess guess={guess} />
+			{Array.from({ length: 5 - submittedGuesses.length }).map((row, idx) => {
+				return <EmptyGuess key={idx} />;
+			})}
 		</div>
 	);
+}
+type CurrentGuessProps = {
+	guess: string[];
+};
+function CurrentGuess({ guess }: CurrentGuessProps) {
+	return (
+		<div className={styles.word}>
+			{/* generate 5 empty boxes and only fill them if there is a guessed letter at that idx */}
+			{Array.from({ length: 5 }).map((letter, idx) => {
+				return (
+					<div className={styles.letterContainer} key={idx}>
+						{guess[idx] || ""}
+					</div>
+				);
+			})}
+		</div>
+	);
+}
+
+function EmptyGuess() {
+	return (
+		<div className={styles.word}>
+			{Array.from({ length: 5 }).map((row, idx) => {
+				return <div className={styles.letterContainer} key={idx}></div>;
+			})}
+		</div>
+	);
+}
+type PreviousGuessesProps = {
+	submittedGuesses: string[][];
+};
+function PreviousGuesses({ submittedGuesses }: PreviousGuessesProps) {
+	return submittedGuesses.map((guess, idx) => {
+		return <div className={styles.word} key={idx}>
+			{guess.map((letter, idx) => {
+				return (
+					<div className={styles.letterContainer} key={idx}>
+						{letter}
+					</div>
+				);
+			})}
+		</div>;
+	});
 }
